0b04603e3932992b42f9e7dfa98b4ed3da51299f
Will McKenzie <willmckenzie@oinutter.co.uk>
2013-01-08 11:15:25 +0000

    Tidy setup.py and increase version number to 0.2

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from distutils.core import setup
 
 setup(name='UgliPyJS',
-	  version='0.1',
-	  description='Python wrapper for Uglify-JS library.',
-	  author='Will McKenzie',
-	  author_email='will@oinutter.co.uk',
-	  packages=['uglipyjs'],
-	  package_dir={'uglipyjs': 'uglipyjs'},
+      version='0.2',
+      description='Python wrapper for Uglify-JS library.',
+      author='Will McKenzie',
+      author_email='will@oinutter.co.uk',
+      packages=['uglipyjs'],
+      package_dir={'uglipyjs': 'uglipyjs'},
       package_data={
-        'uglipyjs': ['*.js'],
-      },
-	  requires=['PyV8','ordereddict','PyExecJS']
-	  )
+       	'uglipyjs': ['*.js'],
+     },
+     requires=['PyV8','ordereddict','PyExecJS']
+     )
