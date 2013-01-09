912c788ed2946e6a05d936581d60cd0eaf674290
OiNutter <willmckenzie@oinutter.co.uk>
2013-01-09 00:47:47 +0000

    Expand setup.py and bump version number to 0.2.1

--- setup.py
+++ setup.py
@@ -1,8 +1,11 @@
 from distutils.core import setup
 
 setup(name='UgliPyJS',
-      version='0.2',
+      version='0.2.1',
+      url='https://github.com/OiNutter/uglipyjs',
+      download_url='https://github.com/OiNutter/uglipyjs/tarball/master',
       description='Python wrapper for Uglify-JS library.',
+      long_description=open('README').read(),
       author='Will McKenzie',
       author_email='will@oinutter.co.uk',
       packages=['uglipyjs'],
@@ -10,5 +13,16 @@ setup(name='UgliPyJS',
       package_data={
        	'uglipyjs': ['*.js'],
      },
-     requires=['PyV8','ordereddict','PyExecJS']
-     )
+     requires=['PyV8','ordereddict','PyExecJS'],
+     license='MIT License',
+     classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2',
+          'Programming Language :: Python :: 2.6',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: JavaScript'
+    ]
+  )
